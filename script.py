ad314d38f190bb41f1d1c6a2140013aeae7e9236
evansmbithi <evholmbithy@gmail.com>
2026-01-21 21:00:46 +0300

    added scroll feature

---
+++ script.py
@@ -0,0 +1,236 @@
+
+# https://github.com/SergeyPirogov/webdriver_manager
+
+import time, sys
+
+from dotenv import dotenv_values
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.edge.service import Service as EdgeService
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+# create a .env file with USERID, PASSWORD and COUNTRY variables
+config = dotenv_values(".env")
+
+# options = webdriver.ChromeOptions()
+options = webdriver.EdgeOptions()
+options.accept_insecure_certs = True # (firefox)
+options.add_argument('--ignore-ssl-errors=yes') #ignore privacy issues
+options.add_argument('--ignore-certificate-errors') #ignore certificate errors (chrome)
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+# try:
+#     mode = sys.argv[3]
+#     if mode.lower() in '--headless=true full':
+#         options.add_argument("--headless=new")
+#         print('--headless=TRUE')
+#     else:
+#         print('--headless=FALSE')       
+# except:
+#     options.add_argument("--headless=new")
+#     print('--headless=TRUE')
+    
+
+# countries = ['rwanda rw','kenya ke','malawi mw','botswana bw','zambia zm']
+
+# try:
+#     cmdline=sys.argv[1]
+#     cmdline2=sys.argv[2]    
+# except:
+#     err_msg = """Please specify country & environment as a commandline argument e.g
+#                 'python index_crn.py RW UAT'
+#                 'python index_crn.py Rwanda PROD'"""
+#     print(err_msg)
+#     exit()
+
+# if cmdline.lower() in 'botswana bw' and cmdline2.lower() in 'production':
+#     hostname = config['BW_PROD']
+# if cmdline.lower() in 'rwanda' and cmdline2.lower() in 'production':
+#     hostname = config['RW_Aos2_PROD']
+# elif cmdline2.lower() in 'production':
+#     hostname = config['PROD']
+# else:
+#     hostname = config['UAT']
+
+# if cmdline not in "".join(countries):
+#     print(f'{cmdline} is not in scope')
+#     exit()
+  
+# driver = webdriver.Edge('../msedgedriver', options=options)
+# driver = webdriver.Edge(service=EdgeService(executable_path=r'C:\Users\eikindu\Downloads\automation\msedgedriver.exe',service_args=['--log-level=DEBUG','--disable-build-check'],log_output='logs'),options=options)
+try:
+    # driver = webdriver.Chrome('../chromedriver',options=options)
+    # driver = webdriver.Edge('../msedgedriver',service=EdgeService(service_args=['--log-level=DEBUG','--disable-build-check'],log_output='logs'),options=options) # Deprecated
+    driver = webdriver.Edge(service=EdgeService(executable_path=r'D:\PROJECTS\the_sweep\msedgedriver.exe',service_args=['--log-level=DEBUG','--disable-build-check'],log_output='logs'),options=options)
+except:
+    print('Trying to fetch web driver...')
+    # driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    # use Edge instead
+    # https://pypi.org/project/webdriver-manager/#use-with-edge
+    try:
+        driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install(),service_args=['--log-level=DEBUG','--disable-build-check'],log_output='logs'),options=options)
+    except:
+        print('Please update msedgedriver on AUTOMATION/ to proceed')
+        exit()
+    
+# wait time before throwing a Timeout Exception 
+driver.implicitly_wait(3) # seconds
+
+# wait for a certain condition to occur before proceeding further in the code
+wait = WebDriverWait(driver, 30) # explicit wait
+
+def check_configs():
+    try:
+        config['USERID']
+        config['PASSWORD']
+    except KeyError:
+        print("Please configure .env with USERID, PASSWORD, PROD and UAT variables")
+        driver.close()
+        exit()
+
+def error_page():
+    try:
+        error_msg = driver.find_element(By.CSS_SELECTOR, "div#content.ng-scope").text
+        content_wrapper = driver.find_element(By.CSS_SELECTOR, "div#content-wrapper").get_attribute("innerHTML")
+    except:
+        return True
+
+    if 'Authentication scheme not supported' in error_msg or content_wrapper == False or content_wrapper == None:
+        return True
+    else:
+        return False
+
+def login():
+
+    check_configs()
+
+    driver.maximize_window()
+    try:
+        # driver.get(f"{hostname}/login")
+        driver.get(f"https://www.instagram.com")
+    except:
+        print('website is not reachable')
+
+    # while error_page():
+    #     driver.refresh() # https://www.codespeedy.com/how-to-refresh-or-reload-a-webpage-in-selenium-python/
+    #     # print('Skipping error page')
+        
+    # time.sleep(2)
+    print('Login page ✅')
+    try:
+        intfc = False
+        while intfc != True:
+            try:
+                # userid = wait.until(EC.visibility_of_element_located((By.name, 'email')))
+                # userid = wait.until(EC.visibility_of_element_located((By.XPATH, '//input[@name="email"]')))
+                userid = wait.until(EC.visibility_of_element_located((By.XPATH, '//input[@name="username"]'))) #
+                intfc = True
+            except:
+                driver.refresh()
+                intfc = False
+            finally:
+                userid = wait.until(EC.visibility_of_element_located((By.XPATH, '//input[@name="username"]'))) #
+                intfc = True
+
+        password = wait.until(EC.visibility_of_element_located((By.XPATH, '//input[@name="password"]'))) #
+
+        # password = wait.until(EC.visibility_of_element_located((By.XPATH, '//input[@name="pass"]')))
+
+        login = wait.until(EC.visibility_of_element_located((By.XPATH, '//button[@type="submit"]'))) #
+
+        # login = wait.until(EC.visibility_of_element_located((By.XPATH, '//div[@role="button"]')))
+    except:
+        print('Elements not found in login page')
+    try:
+        userid.send_keys(config['USERID'])    
+        password.send_keys(config['PASSWORD'])    
+    except:
+        print('Please configure .env with USERID and PASSWORD')    
+
+    time.sleep(3)
+    try:
+        login.click()
+    except:
+        print('login btn unclickable')
+
+    lgn_suc = False
+    while lgn_suc != True:
+        try:
+            wait.until(EC.element_to_be_clickable((By.XPATH, "//a[@href='/_.evnz_/?next=%2F']")))
+            lgn_suc = True
+        except:
+            try:
+                login.click()
+                lgn_suc = False
+            except:
+                print('sth went wrong')
+                exit()
+
+login()
+
+# time.sleep(10)
+try:
+    profile = wait.until(EC.element_to_be_clickable((By.XPATH, "//a[@href='/_.evnz_/?next=%2F']")))
+    profile.click()
+except:
+    print('profile button not found')
+
+print('Profile page ✅')
+time.sleep(3)
+
+try:
+    following = wait.until(EC.element_to_be_clickable((By.XPATH, '//a[@href="/_.evnz_/following/?next=%2F"]')))
+    following.click()
+except:
+    print('profile button not found')
+
+following_count = int(str(following.text).replace('following', '').replace(',','').strip())
+print(following_count)
+
+print('Following dialog ✅')
+
+
+time.sleep(5)
+dialog = wait.until(EC.visibility_of_element_located((By.XPATH, "//div[@class='x6nl9eh x1a5l9x9 x7vuprf x1mg3h75 x1lliihq x1iyjqo2 xs83m0k xz65tgg x1rife3k x1n2onr6']")))
+time.sleep(2)
+
+last_item = False
+while last_item != True:
+    # Method 1: Scroll using JavaScript 
+    for i in range(100): # scroll multiple times 
+        driver.execute_script("arguments[0].scrollTop = arguments[0].scrollTop + 300;", dialog) 
+        time.sleep(2)
+
+    items = dialog.find_elements(By.CSS_SELECTOR, "div > div > div a")
+    print(len(items))
+    if following_count == len(items):
+        print(len(items))
+        last_item = True
+
+print('Scrolling through ✅')
+
+for item in items: 
+    ActionChains(driver).move_to_element(item).click() 
+    time.sleep(0.5)
+    break
+
+
+# # Method 2: Scroll to each item (optional) 
+# items = dialog.find_elements(By.CSS_SELECTOR, ".item-class") # adjust selector 
+# for item in items: 
+#     ActionChains(driver).move_to_element(item).perform() 
+#     time.sleep(0.5)
+
+
+# user_list = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[5]/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/div[1]/div'))) 
+
+
+
+time.sleep(3600)
